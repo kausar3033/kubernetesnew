ecd8a257d32103152ea7bed217d6dcd3bb669af5
kausar <43438240+kausar3033@users.noreply.github.com>
2022-05-14 14:04:44 +0600

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -3,27 +3,22 @@
 #!/bin/sh
 
 # Author : Kausar (iBOS)
-# Copyright (c) Tutorialspoint.com
 # Script follows here:
 ## Remove Docker,kubeadm,kubelet,kubectl
 
+
+sudo systemctl stop kubelet
 sudo systemctl stop docker.socket
-
-sudo apt remove --purge docker-ce docker-ce-cli containerd.io docker*
-
-sudo rm -rf ~/.kube
-
+sudo systemctl stop docker
+sudo apt remove --purge docker-ce docker-ce-cli containerd.io
+sudo apt remove --purge docker*
 sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*
-
+sudo rm -rf ~/.kube
 sudo kubeadm reset
-
 sudo apt-get autoremove
 
-
 sudo apt autoremove -y
-
 sudo apt autoclean
-
 
 
 #Do you want to continue? [Y/n] y
