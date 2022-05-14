44d46dbdd9569fa1c5f2705569bccde4deb78ef0
kausar <43438240+kausar3033@users.noreply.github.com>
2022-05-14 17:48:42 +0600

    update

--- oneclickinstallation/master.sh
+++ oneclickinstallation/master.sh
@@ -1,4 +1,18 @@
+
+apt-get install -y curl openssh-server
+
+
+echo $(hostname -i) $(hostname) >> /etc/hosts
+sudo sed -i "/swap/s/^/#/" /etc/fstab
+sudo swapoff -a
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
 apt-get update
+
 
 apt-get install -y ebtables ethtool
 apt-get install -y docker.io
